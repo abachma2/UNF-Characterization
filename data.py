1ec7d92c703617122e8f9795d407db54285c6630
Amanda Bachmann <abacham2@vols.utk.edu>
2017-05-02 21:31:37 -0400

    added comments

--- data.py
+++ data.py
@@ -1,13 +1,13 @@
 import io
 import numpy as np
 
-BU=[20,25,30,35,40,45,50,55,60]
-EN = np.genfromtxt("EN_values.txt",usecols=(0,1,2,3,4,5))
+BU=[20,25,30,35,40,45,50,55,60] #Burnup values used, these are hardcoded in 
+EN = np.genfromtxt("EN_values.txt",usecols=(0,1,2,3,4,5)) #Enrichment values calculated in origen_script.py and saved to the file specified
 
 CoolingTimes = 6
 initSkip = 6
 numNB = 4 # number of neutron energy bins
-numGB = 201 #number of gamma energy bins
+numGB = 2049 #number of gamma energy bins
 data = np.array([])
 data_string = ''
 
@@ -23,14 +23,14 @@ with io.open("data.txt",'w') as f:
                 gamma_tmp = np.genfromtxt(str(file_name1), skip_header=GSkip, usecols=1, max_rows=numGB)
                 # Take every other row from gamma (due to repeats)
                 data_string = data_string + ' '.join(map(str,gamma_tmp[0::2])) + ' '
-                GSkip = GSkip + numGB + 2
+                GSkip = GSkip + numGB + 2 #Skips to the next cooling time in the flie
                   
                 #Neutron Spectrum
                 file_name2 = "w17x17." + str(BU[x]) + '.' + str(y) + ".000000000000000002.plt"
                 neutron_tmp = np.genfromtxt(str(file_name2), skip_header=NSkip,usecols=1, max_rows=numNB)
                 # Take every other row from neutrons (due to repeats)
                 data_string = data_string + ' '.join(map(str,neutron_tmp[0::2])) + "\n"
-                NSkip = NSkip + numNB + 1
+                NSkip = NSkip + numNB + 1 #Skips to the next cooling time in the file
                     
     f.write(data_string)
 f.close()
--- run_loops.sh
+++ run_loops.sh
@@ -1,3 +1,5 @@
+#This script loops over all .inp files in the directory and runs them through SCALE
+
 for f in *.inp
     do scalerte -m $f file 
 done
